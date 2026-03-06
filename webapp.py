6078719c5bdcd0abfe161839241c8faafd02ed99
TrondCognizant <trondmartin.augutson@cognizant.com>
2026-03-06 15:52:17 +0100

    make only df retractable

--- webapp.py
+++ webapp.py
@@ -26,45 +26,40 @@ try:
 except Exception as e:
     st.error(f"An error occurred: {e}")
     
-
-
 interval_days = st.slider("Select the time interval (days]", min_value=5, max_value=max_value, value=30)
-
     
 # Fetch data button
-if st.sidebar.button("Fetch Data"):
-    with st.spinner(f'Fetching data for {ticker}...'): 
-        data = data_full.iloc[-1*interval_days:]
-        if not data.empty:
-            st.subheader(f"{ticker.upper()} - Last {interval_days} Days")
-            
-            # Create Candlestick chart
-            fig = go.Figure(data=[go.Candlestick(
-                x=data.index,
-                open=data['Open'],
-                high=data['High'],
-                low=data['Low'],
-                close=data['Close'],
-                name=ticker
-            )])
-            
-            # Customize layout
-            fig.update_layout(
-                title=f"{ticker.upper()} Price (Previous Month)",
-                yaxis_title="Price (USD)",
-                xaxis_title="Date",
-                xaxis_rangeslider_visible=False,
-                template="plotly_dark",
-                height=600
-            )
-            
-            # Show plot
-            st.plotly_chart(fig, use_container_width=True)
-    
-         # Optional: Show raw data
+
+with st.spinner(f'Fetching data for {ticker}...'): 
+    data = data_full.iloc[-1*interval_days:]
+    if not data.empty:
+        st.subheader(f"{ticker.upper()} - Last {interval_days} Days")
+        
+        # Create Candlestick chart
+        fig = go.Figure(data=[go.Candlestick(
+            x=data.index,
+            open=data['Open'],
+            high=data['High'],
+            low=data['Low'],
+            close=data['Close'],
+            name=ticker
+        )])
+        
+        # Customize layout
+        fig.update_layout(
+            title=f"{ticker.upper()} Price (Previous Month)",
+            yaxis_title="Price (USD)",
+            xaxis_title="Date",
+            xaxis_rangeslider_visible=False,
+            template="plotly_dark",
+            height=600
+        )
+        # Show plot
+        st.plotly_chart(fig, use_container_width=True)
+        if st.sidebar.button("Show data frame"):
+            # Optional: Show raw data
             st.write(data)
 
-        else:
-            st.error("No data found. Please check the ticker symbol.")           
-else:
-    st.info("Enter a ticker in the sidebar and click 'Fetch Data'.")
+    else:
+        st.error("No data found. Please check the ticker symbol.")           
+
