2afaab099aeddab3f4bb9db28b365a9035476d07
TrondCognizant <trondmartin.augutson@cognizant.com>
2026-03-10 13:46:53 +0100

    bug fix

--- webapp.py
+++ webapp.py
@@ -7,7 +7,7 @@ import streamlit as st
 #st.info("⬅️ Now you can start navigating the tabs in the left-panel menu")
 
 get_live_data = st.Page("pages/page_read_stock_data.py")
-get_data_from_blob = st.Page("pages/page_read_stock_data.py")
+get_data_from_blob = st.Page("pages/page_load_data_from_blob.py")
 
 # pg = st.navigation([read_write_data])
 pg = st.navigation(
