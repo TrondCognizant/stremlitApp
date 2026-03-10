5481bafc9d258a28107e57ab4ba34fb505ebb05c
TrondCognizant <trondmartin.augutson@cognizant.com>
2026-03-10 13:31:04 +0100

    testing small layout change

--- webapp.py
+++ webapp.py
@@ -1,17 +1,18 @@
 import streamlit as st
 
 # Set page config
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 
-st.markdown("### **Homepage of stockdata processing **")
-st.info("⬅️ Now you can start navigating the tabs in the left-panel menu")
+#st.markdown("### **Homepage of stockdata processing **")
+#st.info("⬅️ Now you can start navigating the tabs in the left-panel menu")
 
 read_write_data = st.Page("pages/page_read_stock_data.py", title="Get Data")
 
 # pg = st.navigation([read_write_data])
 pg = st.navigation(
     {
-        "Get Stock Data": [read_write_data]
+        "Get Stock Data": [read_write_data],
+         "Get Stock Data2": [read_write_data],
     }
 )
 pg.run()
