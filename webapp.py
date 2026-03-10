cdb2baf769038edefaa9d108ece7f10fa1c8db21
TrondCognizant <trondmartin.augutson@cognizant.com>
2026-03-10 13:22:53 +0100

    changed page structure

--- webapp.py
+++ webapp.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 st.markdown("### **Homepage of stockdata processing **")
 st.info("⬅️ Now you can start navigating the tabs in the left-panel menu")
 
-read_write_data = st.Page("page_read_stock_data.py", title="Get Data")
+read_write_data = st.Page("pages/page_read_stock_data.py", title="Get Data")
 
 # pg = st.navigation([read_write_data])
 pg = st.navigation(
