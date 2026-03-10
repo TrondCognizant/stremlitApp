a1bc1dc0a0f055ef91da2d6f5825c8bbbedf547f
TrondCognizant <trondmartin.augutson@cognizant.com>
2026-03-10 13:55:01 +0100

    re arranged pages

--- webapp.py
+++ webapp.py
@@ -6,14 +6,14 @@ import streamlit as st
 #st.markdown("### **Homepage of stockdata processing **")
 #st.info("⬅️ Now you can start navigating the tabs in the left-panel menu")
 
-get_live_data = st.Page("pages/page_read_stock_data.py")
 get_data_from_blob = st.Page("pages/page_load_data_from_blob.py")
+get_live_data = st.Page("pages/page_read_stock_data.py")
+
 
 # pg = st.navigation([read_write_data])
 pg = st.navigation(
     {
-        "Get Live Data": [get_live_data],
-        "Get Data from Blob": [get_data_from_blob]
+        "Get Data": [get_data_from_blob, get_live_data]
     }
 )
 pg.run()
