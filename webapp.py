aee2ca84f6903749ed0784ceeed52f76ba7aab1e
TrondCognizant <trondmartin.augutson@cognizant.com>
2026-03-10 13:03:41 +0100

    added gp.run()

--- webapp.py
+++ webapp.py
@@ -8,11 +8,13 @@ st.info("⬅️ Now you can start navigating the tabs in the left-panel menu")
 
 read_write_data = st.Page("page_read_stock_data.py", title="Get Data")
 
-pg = st.navigation(
-    {
-        "Get Stock Data": [read_write_data]
-    }
-)
+pg = st.navigation([read_write_data])
+#pg = st.navigation(
+#    {
+#        "Get Stock Data": [read_write_data]
+#    }
+#)
 
+pg.run()
 
 
