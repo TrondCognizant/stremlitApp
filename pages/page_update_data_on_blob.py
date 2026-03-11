13bde1f4ab83a356cd89f4e9d7a6068c62493f12
TrondCognizant <trondmartin.augutson@cognizant.com>
2026-03-11 13:30:07 +0100

    updated data upload page

--- pages/page_update_data_on_blob.py
+++ pages/page_update_data_on_blob.py
@@ -2,12 +2,12 @@ from read_data import update_and_save_to_azure
 import streamlit as st
 
 # Setup connection (Use environment variables for production!)
-account_url = "https://genaitraining68696287510.blob.core.windows.net"
-container_name = "stock-data"
-blob_name = "Etoro_2000_14mar2025.parquet" # Or prices.csv
+# account_url = "https://genaitraining68696287510.blob.core.windows.net"
+# container_name = "stock-data"
+# blob_name = "Etoro_2000_14mar2025.parquet" # Or prices.csv
 
 
 st.title("Load and Update Historic Stock Data")
 if st.button("Start stock data update"):
-    update_and_save_to_azure(container_name, blob_name, batch_size=30, pause_seconds=2)
+    update_and_save_to_azure(batch_size=30, pause_seconds=2) # container_name, blob_name, 
 
