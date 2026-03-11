f354d6edc9a4dbe6b090f972165b9a077e92d8fb
TrondCognizant <trondmartin.augutson@cognizant.com>
2026-03-11 14:13:10 +0100

    added select box to data from blob

--- pages/page_load_data_from_blob.py
+++ pages/page_load_data_from_blob.py
@@ -4,8 +4,16 @@ import streamlit as st
 
 st.title("Load Historic Stock Data")
 df_loaded = load_stock_data()
+all_tickers = list(set(df_loaded.columns.get_level_values(0)))
+ticker = st.selectbox(
+    "Search and select a stock:",
+    all_tickers,
+    index=None,         # Starts with nothing selected
+    placeholder="Type to search...",
+)
 
-ticker = st.text_input("Enter Stock Ticker", value="Select ticker")
+st.write(f"You selected: {ticker}")
+# ticker = st.text_input("Enter Stock Ticker", value="Select ticker")
 
 if (ticker != "Select ticker"):
     st.write("Preview of Stock Data")
